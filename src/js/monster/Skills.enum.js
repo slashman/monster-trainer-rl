eb16f1a7a9d225bbd2c605cc5a405052a17fa6ed
slash <slash@slashware.net>
2017-03-13 01:02:52 -0500

    Real data WIP

--- src/js/monster/Skills.enum.js
+++ src/js/monster/Skills.enum.js
@@ -3,48 +3,9 @@ var Effects = require('./Effects.enum');
 var Stats = require('./Stats.enum');
 
 module.exports = {
-	TACKLE: {
-		name: 'Tackle',
-		type: Types.NORMAL,
-		power: 40,
-		accuracy: 100,
-		effect: Effects.DAMAGE,
-		damageType: 'PHYS',
-		pp: 35
-	},
-	SCRATCH: {
-		name: 'Scratch',
-		type: Types.NORMAL,
-		power: 40,
-		accuracy: 100,
-		effect: Effects.DAMAGE,
-		damageType: 'PHYS',
-		pp: 35
-	},
-	TAIL_WHIP: {
-		name: 'Tail Whip',
-		type: Types.NORMAL,
-		accuracy: 100,
-		effect: Effects.LOWER_STAT,
-		stat: Stats.DEFENSE,
-		pp: 30
-	},
-	GROWL: {
-		name: 'Growl',
-		type: Types.NORMAL,
-		accuracy: 100,
-		effect: Effects.LOWER_STAT,
-		stat: Stats.ATTACK,
-		pp: 40
-	},
-	EMBER: {
-		name: 'Ember',
-		type: Types.FIRE,
-		power: 40,
-		accuracy: 100,
-		effect: Effects.DAMAGE,
-		damageType: 'SPEC',
-		pp: 25,
-		range: 3
-	}
+TACKLE: {name: 'Tackle', type: Types.NORMAL, power: 40, accuracy: 100, effect: Effects.DAMAGE, pp: 35, damagetype: 'PHYS'},
+SCRATCH: {name: 'Scratch', type: Types.NORMAL, power: 40, accuracy: 100, effect: Effects.DAMAGE, pp: 35, damagetype: 'PHYS'},
+TAIL_WHIP: {name: 'Tail Whip', type: Types.NORMAL, power: 0, accuracy: 100, effect: Effects.LOWER_STAT, pp: 30, stat: Stats.DEFENSE},
+GROWL: {name: 'Growl', type: Types.NORMAL, power: 0, accuracy: 100, effect: Effects.LOWER_STAT, pp: 40, stat: Stats.ATTACK},
+EMBER: {name: 'Ember', type: Types.FIRE, power: 40, accuracy: 100, effect: Effects.DAMAGE, pp: 25, damagetype: 'SPEC', range: 3}
 }
