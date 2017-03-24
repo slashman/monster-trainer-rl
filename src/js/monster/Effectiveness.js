b9ec9751f2ab1da9233d4e611655f60c03ae1b47
slash <slash@slashware.net>
2017-03-24 01:13:01 -0500

    Fix type damage modifiers, add complete typextype table, add secondary types

--- src/js/monster/Effectiveness.js
+++ src/js/monster/Effectiveness.js
@@ -1,40 +1,126 @@
 module.exports = function(skill, monster, target){
-	if (modifiers[skill.type] && modifiers[skill.type][target.type]){
-		return modifiers[skill.type][target.type];
+	if (modifiers[skill.type.id]){
+		if (modifiers[skill.type.id][target.race.type.id])
+			return modifiers[skill.type.id][target.race.type.id];
+		if (target.race.typeb && modifiers[skill.type.id][target.race.typeb.id])
+			return modifiers[skill.type.id][target.race.typeb.id];
+		return 1;
 	} else {
 		return 1;
 	}
 }
 
 var modifiers = {
-	Normal: {
-		Rock: 0.5,
-		Ghost: 0
+	NORMAL: {
+		ROCK: 0.5,
+		GHOST: 0
 	},
-	Fight: {
-		Normal: 2,
-		Flying: 0.5,
-		Poison: 0.5,
-		Rock: 2,
-		Bug: 0.5,
-		Ghost: 0,
-		Psychic: 0.5,
-		Ice: 2
+	FIGHTING: {
+		NORMAL: 2,
+		FLYING: 0.5,
+		POISON: 0.5,
+		ROCK: 2,
+		BUG: 0.5,
+		GHOST: 0,
+		PSYCHIC: 0.5,
+		ICE: 2
 	},
-	Flying: {
-		Fight: 2,
-		Rock: 0.5,
-		Bug: 2,
-		Grass: 2,
-		Electric: 0.5
+	FLYING: {
+		FIGHTING: 2,
+		ROCK: 0.5,
+		BUG: 2,
+		GRASS: 2,
+		ELECTRIC: 0.5
 	},
-	Fire: {
-		Rock: 0.5,
-		Bug: 2,
-		Fire: 0.5, 
-		Water: 0.5,
-		Grass: 2,
-		Ice: 2,
-		Dragon: 0.5
+	POISON: {
+		POISON: 0.5,
+		GROUND: 0.5,
+		ROCK: 0.5,
+		BUG: 2,
+		GHOST: 0.5,
+		GRASS: 2
+	},
+	GROUND: {
+		FLYING: 0,
+		POISON: 2,
+		ROCK: 2,
+		BUG: 0.5,
+		FIRE: 2,
+		GRASS: 0.5,
+		ELECTRIC: 2
+	},
+	ROCK: {
+		FIGHTING: 0.5,
+		FLYING: 2,
+		GROUND: 0.5,
+		BUG: 2,
+		FIRE: 2,
+		ICE: 2
+	},
+	BUG: {
+		FIGHTING: 0.5,
+		FLYING: 0.5,
+		POISON: 2,
+		GHOST: 0.5,
+		FIRE: 0.5,
+		GRASS: 2,
+		PSYCHIC: 2
+	},
+	GHOST: {
+		NORMAL: 0,
+		GHOST: 2,
+		PSYCHIC: 0
+	},
+	FIRE: {
+		ROCK: 0.5,
+		BUG: 2,
+		FIRE: 0.5, 
+		WATER: 0.5,
+		GRASS: 2,
+		ICE: 2,
+		DRAGON: 0.5
+	},
+	WATER: {
+		GROUND: 2,
+		ROCK: 2,
+		FIRE: 2,
+		WATER: 0.5,
+		GRASS: 0.5,
+		DRAGON: 0.5
+	},
+	GRASS: {
+		FLYING: 0.5,
+		POISON: 0.5,
+		GROUND: 2,
+		ROCK: 2,
+		BUG: 0.5,
+		FIRE: 0.5,
+		WATER: 2,
+		GRASS: 0.5,
+		DRAGON: 0.5
+	},
+	ELECTRIC: {
+		FLYING: 2,
+		GROUND: 0,
+		WATER: 2,
+		GRASS: 0.5,
+		ELECTRIC: 0.5,
+		DRAGON: 0.5
+	},
+	PSYCHIC: {
+		FIGHTING: 2,
+		POISON: 2,
+		PSYCHIC: 0.5,
+	},
+	ICE: {
+		FLYING: 2,
+		GROUND: 2,
+		WATER: 0.5,
+		GRASS: 2,
+		ICE: 0.5,
+		DRAGON: 2
+	},
+	DRAGON: {
+		DRAGON: 2
 	}
 }
--- src/js/monster/Types.enum.js
+++ src/js/monster/Types.enum.js
@@ -1,4 +1,47 @@
 module.exports = {
-	NORMAL: {},
-	FIRE: {}
+	NORMAL: {
+		id: 'NORMAL'
+	},
+	FIGHTING: {
+		id: 'FIGHTING'
+	},
+	FLYING: {
+		id: 'FLYING'
+	},
+	POISON: {
+		id: 'POISON'
+	},
+	GROUND: {
+		id: 'GROUND'
+	},
+	ROCK: {
+		id: 'ROCK'
+	},
+	BUG: {
+		id: 'BUG'
+	},
+	GHOST: {
+		id: 'GHOST'
+	},
+	FIRE: {
+		id: 'FIRE'
+	},
+	WATER: {
+		id: 'WATER'
+	},
+	GRASS: {
+		id: 'GRASS'
+	},
+	ELECTRIC: {
+		id: 'ELECTRIC'
+	},
+	PSYCHIC: {
+		id: 'PSYCHIC'
+	},
+	ICE: {
+		id: 'ICE'
+	},
+	DRAGON: {
+		id: 'DRAGON'
+	}
 }
